e7890281ead0c57b9fff3d774d731091ddf9ad53
Alex <aleksandr.leontev.n@mail.ru>
2022-11-07 21:31:48 +0300

    fix comment

--- classes/databaseOperations.php
+++ classes/databaseOperations.php
@@ -1,11 +1,10 @@
 <?php
 require_once("classes/Database.php");
-class CalculatorAndDb {
+class databaseOperations {
 public function __construct() {
 Database::connect();
 }
     public function saveResultBd($number1, $number2, $operation, $result) {
-
             Database::query("INSERT INTO numbers (operand_1, operator, operand_2, result)
                              VALUES ( '" . $_REQUEST['input-one'] . "',
                                       '" . $_REQUEST['operation'] . "',
